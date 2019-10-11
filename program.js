ecf288bab12a550d53857c774f0a55d2d105238b
Daniel Minschinski <danielminschinski@gmail.com>
2019-10-11 20:51:52 +0200

    Create program.js

---
+++ program.js
@@ -0,0 +1,23 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function(){
+	var input = process.stdin.read();
+	if(input !== null){
+		var instruction = input.toString().trim();
+		switch(instruction){
+			case '/exit':
+				process.stdout.write('Quitting app!\n');
+				process.exit();
+				break;
+			case '/version_Node':
+				console.log(process.versions.node);
+				break;
+			case '/lang_Node':
+				console.log(process.env.lang);
+				break;
+			default:
+				process.stderr.write('Wrong instruction!');
+		}
+	}		
+			
+});
