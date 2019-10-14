4fb33276278c59fa1463e5686ee6f9855198c5a8
Daniel Minschinski <danielminschinski@gmail.com>
2019-10-14 19:34:28 +0200

    Update program.js

--- program.js
+++ program.js
@@ -1,23 +1,25 @@
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', function(){
-	var input = process.stdin.read();
-	if(input !== null){
-		var instruction = input.toString().trim();
-		switch(instruction){
-			case '/exit':
-				process.stdout.write('Quitting app!\n');
-				process.exit();
-				break;
-			case '/version_Node':
-				console.log(process.versions.node);
-				break;
-			case '/lang_Node':
-				console.log(process.env.lang);
-				break;
-			default:
-				process.stderr.write('Wrong instruction!');
-		}
-	}		
-			
+    var input = process.stdin.read();
+    if(input !== null){
+        var instruction = input.toString().trim();
+        switch(instruction){
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            case '/version_Node':
+                process.stdout.write('Version: ' + process.versions.node + '\n');
+                process.stdin.resume();
+                break;
+            case '/lang_Node':
+                process.stdout.write('Lang: ' + process.env.lang + '\n');
+                process.stdin.resume();
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
+                process.stdin.resume();
+        }
+    }                
 });
